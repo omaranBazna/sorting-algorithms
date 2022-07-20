b667aa234591fa6583bf79db2caf33827581bc58
omaranBazna <omran4d@gmail.com>
2022-07-19 23:53:53 -0400

    test the insertion sort

--- inserting.js
+++ inserting.js
@@ -1,13 +1,16 @@
 let arr = [1, 2, 4, -20, 20, 1, 2, 4];
 
 let index = 0;
-let min = Infinity;
-let minIndex = -1;
-for (let i = index; i < arr.length; i++) {
-  if (arr[i] < min) {
-    min = arr[i];
-    minIndex = i;
+for (let i in arr) {
+  let min = Infinity;
+  let minIndex = -1;
+  for (let i = index; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+      minIndex = i;
+    }
   }
+  [arr[index], arr[minIndex]] = [arr[minIndex], arr[index]];
+  index++;
 }
-[arr[index], arr[minIndex]] = [arr[minIndex], arr[index]];
-index++;
+console.log(arr);
