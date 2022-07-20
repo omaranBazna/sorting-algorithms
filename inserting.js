e6aed5172319c3ad2b8e9fd9375ea4a6ce2307cd
omaranBazna <omran4d@gmail.com>
2022-07-19 23:53:07 -0400

    implementing the for loop for selection loop

--- inserting.js
+++ inserting.js
@@ -3,10 +3,11 @@ let arr = [1, 2, 4, -20, 20, 1, 2, 4];
 let index = 0;
 let min = Infinity;
 let minIndex = -1;
-for (let i in arr) {
+for (let i = index; i < arr.length; i++) {
   if (arr[i] < min) {
     min = arr[i];
     minIndex = i;
   }
 }
 [arr[index], arr[minIndex]] = [arr[minIndex], arr[index]];
+index++;
