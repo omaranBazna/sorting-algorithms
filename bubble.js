e97d9ed7f72c6e2317b12c34596c9a974ed0ac16
omaranBazna <omran4d@gmail.com>
2022-07-19 23:39:32 -0400

    swap elements

--- bubble.js
+++ bubble.js
@@ -3,5 +3,8 @@ let arr = [2, 4, 5, 2, 1, 2, 5];
 ///Time Complexity O(n*n)
 for (let i of arr) {
   for (let j of arr) {
+    if (arr[j] < arr[i]) {
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
   }
 }
