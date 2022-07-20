b7b464c76d5bcb6911625692b223147f94dce68b
omaranBazna <omran4d@gmail.com>
2022-07-19 23:44:38 -0400

    implementing selection sort

--- selection.js
+++ selection.js
@@ -1,1 +1,11 @@
 let arr = [4, 2, 0, 1, 5, 10, 12, 11];
+
+let result = [];
+for (let u in arr) {
+  let min = Infinity;
+  for (let i of arr) {
+    min = Math.min(min, i);
+  }
+  result.push(min);
+  arr.splice(arr.indexOf(min), 1);
+}
